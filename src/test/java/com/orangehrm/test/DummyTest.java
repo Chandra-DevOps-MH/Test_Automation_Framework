c60ab176c978c9e86e283b79ea854d322f220c11
chandra <chandrakantghasti99@gmail.com>
2026-01-20 01:25:51 +0530

    some changes to check Jenkins autobuild when push code on github2

--- src/test/java/com/orangehrm/test/DummyTest.java
+++ src/test/java/com/orangehrm/test/DummyTest.java
@@ -12,6 +12,7 @@ public class DummyTest extends BaseClass{
 	public void dummyTest() {
 		
 			//Some changes to check jenkins build auto run when we push code
+		//Some changes to check jenkins build auto run when we push code
 		    ExtendManager.startTest("Start the dummyTest"); //-- This has been implemented in Testlistener class
 			String Title = getDriver().getTitle();
 			ExtendManager.logStep("Verifying the OrangeHRM title");
